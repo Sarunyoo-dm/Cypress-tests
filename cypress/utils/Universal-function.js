5eb81380248c5a772c6e577f611c3aa29f1063e4
Diamond <sarunyoochimmalee@gmail.com>
2025-03-12 09:28:26 +0700

    Adjust Function

---
+++ cypress/utils/Universal-function.js
@@ -0,0 +1,45 @@
+import * as variables from '../utils/variables';
+Object.assign(global,variables)
+
+export function TabDashboard() { // เปิด Dashboard
+    cy.get('.wrap-sidenav-desktop >:nth-child(1)').click()
+  }
+
+export function TabPaymentlink() { // เปิด Paymentlink
+   // cy.get('.wrap-sidenav-desktop >:nth-child(2)').trigger('mouseover')
+    cy.get('.wrap-sidenav-desktop >:nth-child(2)').realHover()
+    cy.get('.wrap-sidenav-desktop >:nth-child(2)').click()
+  }
+
+export function TabComPanyTxn() { // เปิด Companytransaction
+    cy.get('.wrap-sidenav-desktop >:nth-child(3)').click()
+  }
+
+export function TabReport() { // เปิด Report
+    cy.get('.wrap-sidenav-desktop >:nth-child(4)').click()
+  }
+
+export function TabShopSetting() { // เปิด Shop
+    cy.get('.wrap-sidenav-desktop >:nth-child(5)').click()
+  }
+
+export function SubMenuDraft() { // เปิด Draft
+    cy.get('.wrap-desktop-sub-menu ul > li:nth-child(2)').click()
+  }
+
+export function SubMenuPending() { // เปิด Pending
+    cy.get('.wrap-desktop-sub-menu ul > li:nth-child(3)').click()
+  }
+
+  export function SubMenuSchedule() { // เปิด Schedule
+   cy.get('.wrap-desktop-sub-menu ul > li:nth-child(4)').realHover().click()
+   //cy.get('.wrap-desktop-sub-menu ul > li:nth-child(4)').click()
+  }
+  export function SubMenuWaiting() { // เปิด Waiting for approval
+    cy.get('.wrap-desktop-sub-menu ul > li:nth-child(5)').realHover().click()
+  }
+
+  export function ChangeProfile(Profile) { // เลือก Profile
+    cy.get('.wrap-header .wrap-select-class').click()
+    cy.get('.cdk-virtual-scroll-content-wrapper p').contains(Profile).click()
+  }
