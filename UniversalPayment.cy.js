a4be1f114963dbda63c08a20698a20a86a4a5a09
Diamond <sarunyoochimmalee@gmail.com>
2025-03-19 10:07:02 +0700

    Universal Smoke

--- UniversalPayment.cy.js
+++ UniversalPayment.cy.js
@@ -1,5 +1,5 @@
-import 'cypress-real-events/support'
-import * as functions from '../../utils/Universal-function';
+import 'cypress-real-events/support';
+import * as functions from '../../utils/Universal-functions';
 import * as variables from '../../utils/variables';
 Object.assign(global,functions)
 Object.assign(global,variables)
@@ -7,23 +7,30 @@ Object.assign(global,variables)
 describe('template spec', () => {
 
   beforeEach(() => {
-    cy.visit('https://upayment.tbs.prior-dev.app/web/main-content')
+    cy.visit('../../../cypress/e2e/Cypress-tests/Universal_Payment_SAME_PAGE.html')
     cy.viewport(1440, 900);
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as('windowOpen');
+    }); // ใช้เพื่อปิด popup  
   });
+  
   
 
   it('Entry UniversalPayment', () => {
+    ChooseMaker('MAKER 2')
     TabPaymentlink()
-    cy.log('Click Paymentlink')
-    SubMenuWaiting()
+    cy.log('Payment link123')
+    SubMenuDraft()
   })
 
   it('Change Profile',()=>{
-    ChangeProfile('Profile Group C')
+    ChooseMaker('MAKER 2')
+    ChangeProfile('MK Suki 2')
   })
 
-  it.only('Button Action' , () =>{
-   // cy.get('.wrap-card-class > .flex > :nth-child(1)').click() //Route popup
-    cy.get('.wrap-card-class > .flex > :nth-child(2)').click() //Spinner
+  it('Go TabComapny' , () =>{
+    ChooseMaker('MAKER 2')
+    TabComPanyTxn()
+    cy.log('Entry TabCompany')
   })
 })
